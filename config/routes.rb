073a1ce6ed3ad90785070c6902127cc1d98bf215
tomomik9 <tomomi.kanaz@gmail.com>
2019-05-12 22:20:04 +0900

    devise実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+   }
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "(:locale)" do
    resources :books
- end
+  end
+  if Rails.env.development? #開発環境の場合
+   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
